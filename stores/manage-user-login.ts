22792c4e4dbca7bc568a4a777cc28a26a78f9223
Yeelalor <117246201+Yeelalor@users.noreply.github.com>
2025-12-25 17:24:11 +0700

    update login

--- stores/manage-user-login.ts
+++ stores/manage-user-login.ts
@@ -5,4 +5,24 @@ export const useLogin = defineStore("userLogin", {
     passWord: null,
     customerUserData: [],
   }),
+  actions: {
+    allApi() {
+      const { mainApi } = useApi();
+      return mainApi;
+    },
+    async userLoginApi() {
+      var body = {
+        "userLogin": this.userLogin,
+        "passWord": this.passWord
+      }
+      const res = await this.allApi().post("getCustomerByUserAndPass", body);
+      if (res.status === 200) {
+        this.customerUserData = res.data;
+        return this.customerUserData;
+      } else {
+
+        this.customerUserData = [];
+      }
+    },
+  }
 });
